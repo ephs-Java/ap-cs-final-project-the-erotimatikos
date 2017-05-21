f3597a0ca092799ae1a767267ae3862de28869c0
90301472 <90301472@hs-06760-macair.ad.edenpr.org>
2017-05-21 15:04:05 -0500

    Fixed Goomba movement. Found a goomba pictura and added it.

--- src/MarioGame/enemys.java
+++ src/MarioGame/enemys.java
@@ -4,19 +4,30 @@ import java.util.ArrayList;
 
 public class enemys {
 
-	private int flip = 1;
-	ArrayList<enemy> enemyList = new ArrayList<enemy>();
 	
+	
+	ArrayList<Integer> flippers = new ArrayList<Integer>();
+	
+	ArrayList<enemy> enemyList = new ArrayList<enemy>();
+	int first = 0;
+	int flip;
 	public void add(int x, int y){
 		enemyList.add(new enemy(x,y));
 	}
 	
 	public void updateAll(brick[][] arr){
 		
+		if(first == 0){
+			for(int i = 0;i<enemyList.size();i++){
+				flippers.add(1);
+			}
+		}
+		first++;
 		for(int i = 0;i<enemyList.size();i++){
+			flip = flippers.get(i);
 		if(arr[enemyList.get(i).xE+flip][enemyList.get(i).yE].type == 1){
 			
-			flip = flip * -1;
+			flip = flippers.set(i,flip * -1);
 		}
 		else{
 			enemyList.get(i).xE+=flip;
--- src/MarioGame/marioScreen.java
+++ src/MarioGame/marioScreen.java
@@ -17,36 +17,43 @@ import javax.swing.JFrame;
 
 public class marioScreen extends JFrame implements Runnable {
 
-field Field = new field();
-enemys ene = new enemys();
-	
+	field Field = new field();
+	enemys ene = new enemys();
+
 	private Graphics dbg;;
 	private Image dbImage;
 	//35 r by 20c
 	int screenX = 1050,screenY = 630, rowLeng = Field.rowLeng, colLeng = Field.colLeng ;
 	int x,y,yDirect,xDirect,cordX,cordY;
-	
-	
+
+
+	//Enemys will move on odds
+	int enemySpeedGovenor = 0;
 	Image brick;
-	
-	
-	
-	
+	Image goomba;
+
+
+
+
 	public static void main(String[] args) throws FileNotFoundException{
 		marioScreen game = new marioScreen();
 		Thread t = new Thread(game);
 		t.start();
 	}
-	
-	
-public marioScreen() throws FileNotFoundException{
-	
-	importer();
-	enemyFinder(); 
+
+
+	public marioScreen() throws FileNotFoundException{
+
+		importer();
+		enemyFinder(); 
 		x = 30;
 		y=30;
-	ImageIcon brickk = new ImageIcon("src/MarioGame/brick.png");
-	brick = brickk.getImage();
+		ImageIcon brickk = new ImageIcon("src/MarioGame/brick.png");
+		brick = brickk.getImage();
+
+		
+		ImageIcon goom = new ImageIcon("src/MarioGame/goomb.png");
+		goomba = goom.getImage();
 		
 		addKeyListener(new AL());
 		setTitle("MarioGame");
@@ -57,167 +64,168 @@ public marioScreen() throws FileNotFoundException{
 		setBackground(Color.cyan); //Sets the background color to blue.
 
 	}
-public void run(){
-	while(true){
-		try{
-			
-			ene.updateAll(Field.array);
-			if(xDirect > 30){
-				xDirect = 30;
+	public void run(){
+		while(true){
+			try{
+				if(enemySpeedGovenor % 3 == 0)
+					ene.updateAll(Field.array);
+				enemySpeedGovenor++;
+				if(xDirect > 30){
+					xDirect = 30;
+				}
+
+				cordX = x/30;
+				cordY = y /30;
+				move();
+
+				Thread.sleep(100);
+
 			}
-			
-			cordX = x/30;
-			cordY = y /30;
-			move();
-			
-			Thread.sleep(100);
-			
+			catch(Exception e){
+				System.out.println("Error");
+				System.out.println(e.getMessage());
+			}
+
 		}
-		catch(Exception e){
-			System.out.println("Error");
-			System.out.println(e.getMessage());
- }
-	
-}
-}
-	
+	}
+
 	public void importer() throws FileNotFoundException{
 		brick[][] ne = new brick[rowLeng][colLeng]; //Creates a new arraylist of Strings called allWords
 		//scan in the words, one on each line
 		Scanner input = new Scanner(new File("src/MarioGame/level1.txt"));
-		
+
 		for(int c = 0;c<colLeng;c++){
-		for(int r = 0;r<rowLeng;r++){
-		
+			for(int r = 0;r<rowLeng;r++){
+
 				int typer = input.nextInt();
 				ne[r][c] = new brick(typer);
 			}
-			
+
 		}
-		
-		
-		
-			
-		
-	
-	Field.array = ne;
-	
+
+
+
+
+
+
+		Field.array = ne;
+
 	}
 
-public void move(){
-	 if(Field.array[cordX + xDirect/30][cordY].type == 1){
-		
-		 
-	 }else{
-	x+= xDirect;
-}
-if(Field.array[cordX][cordY + yDirect/30].type == 1){
-		
-		 
-	 }else{
-	y+= yDirect;
-}
-	
-	if(y>570){
-		y = 570;
-	}
-	if(y<0){
-		y=0;
-	}
-	
-}
-public void setXDirect(int n){
-	xDirect = n;
-}
-public void setYDirect(int n){
-	yDirect = n;
-}
+	public void move(){
+		if(Field.array[cordX + xDirect/30][cordY].type == 1){
 
-public class AL extends KeyAdapter{
-	
-	public void keyPressed(KeyEvent e){
-		int code = e.getKeyCode();
-		
-		if(code == e.VK_LEFT){
-			
-			setXDirect(-30);
+
+		}else{
+			x+= xDirect;
 		}
-		if(code == e.VK_RIGHT){
-		
-			setXDirect(30);
+		if(Field.array[cordX][cordY + yDirect/30].type == 1){
+
+
+		}else{
+			y+= yDirect;
 		}
-		if(code == e.VK_UP){
-			
-			setYDirect(-30);
+
+		if(y>570){
+			y = 570;
 		}
-		if(code == e.VK_DOWN){
-			
-			setYDirect(30);
+		if(y<0){
+			y=0;
 		}
-		if(code == e.VK_I){
-			try {
-				importer();
-				
-			} catch (FileNotFoundException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
+
+	}
+	public void setXDirect(int n){
+		xDirect = n;
+	}
+	public void setYDirect(int n){
+		yDirect = n;
+	}
+
+	public class AL extends KeyAdapter{
+
+		public void keyPressed(KeyEvent e){
+			int code = e.getKeyCode();
+
+			if(code == e.VK_LEFT){
+
+				setXDirect(-30);
+			}
+			if(code == e.VK_RIGHT){
+
+				setXDirect(30);
+			}
+			if(code == e.VK_UP){
+
+				setYDirect(-30);
+			}
+			if(code == e.VK_DOWN){
+
+				setYDirect(30);
+			}
+			if(code == e.VK_I){
+				try {
+					importer();
+
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 			}
 		}
-		}
-		
-		
-		
-	
-	public void keyReleased(KeyEvent e){
-		int code = e.getKeyCode();
-if(code == e.VK_LEFT){
-	setXDirect(0);
-		}
-		if(code == e.VK_RIGHT){
-			setXDirect(0);
-		}
-		if(code == e.VK_UP){
-			setYDirect(0);
-		}
-		if(code == e.VK_DOWN){
-			setYDirect(0);
-		}
-		
-	}
-}
 
 
 
-public void enemyFinder(){
-	for(int c = 0;c<colLeng;c++){
-	for(int r = 0;r<rowLeng;r++){
-	
-		if(Field.array[r][c].type ==9){
-			ene.add(r, c);
-			Field.array[r][c].type = 0;
+
+		public void keyReleased(KeyEvent e){
+			int code = e.getKeyCode();
+			if(code == e.VK_LEFT){
+				setXDirect(0);
+			}
+			if(code == e.VK_RIGHT){
+				setXDirect(0);
+			}
+			if(code == e.VK_UP){
+				setYDirect(0);
+			}
+			if(code == e.VK_DOWN){
+				setYDirect(0);
+			}
+
 		}
 	}
-	
+
+
+
+	public void enemyFinder(){
+		for(int c = 0;c<colLeng;c++){
+			for(int r = 0;r<rowLeng;r++){
+
+				if(Field.array[r][c].type ==9){
+					ene.add(r, c);
+					Field.array[r][c].type = 0;
+				}
+			}
+
+		}
 	}
-}
-public void paint(Graphics g){
-	dbImage = createImage(getWidth(), getHeight());
-	dbg = dbImage.getGraphics();
-	paintComponent(dbg);
-	g.drawImage(dbImage, 0, 0, this);
-}
-public void paintComponent(Graphics g){
-	
-	for(int r = 0;r<rowLeng;r++){
-	for(int c = 0;c<colLeng;c++){
-		
-			
+	public void paint(Graphics g){
+		dbImage = createImage(getWidth(), getHeight());
+		dbg = dbImage.getGraphics();
+		paintComponent(dbg);
+		g.drawImage(dbImage, 0, 0, this);
+	}
+	public void paintComponent(Graphics g){
+
+		for(int r = 0;r<rowLeng;r++){
+			for(int c = 0;c<colLeng;c++){
+
+
 				if(Field.array[r][c].getType() == 0){
 					g.setColor(Color.red);
-				
-				g.fillRect(r*30, c*30, 30,30);
-				g.setColor(Color.BLACK);
-				g.drawRect(r*30, c*30, 30, 30);
+
+					g.fillRect(r*30, c*30, 30,30);
+					g.setColor(Color.BLACK);
+					g.drawRect(r*30, c*30, 30, 30);
 				}
 				if(Field.array[r][c].getType() == 1){
 					g.drawImage(brick,r *30,c*30, this);
@@ -231,11 +239,15 @@ public void paintComponent(Graphics g){
 	g.setColor(Color.blue);
 	g.fillRect(x, y, 30, 30);
 	
+	
+		
 	for(int i = 0; i < ene.enemyList.size();i++){
 		g.setColor(Color.green);
-		g.fillRect(ene.enemyList.get(i).xE *30, ene.enemyList.get(i).yE*30, 30, 30);
+		g.drawImage(goomba, ene.enemyList.get(i).xE *30, ene.enemyList.get(i).yE * 30
+				, this);
+//		g.fillRect(ene.enemyList.get(i).xE *30, *30, 30, 30);
 	}
-
+	
 	
 		g.drawString("T" + Field.array[cordX][cordY].type,30,615);
 	
